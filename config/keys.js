b689801facbaddda577b3ff935f6b5316f82bda6
ml20 <mounirlalami@hotmail.com>
2021-03-12 01:26:48 +0100

    return to backup 2

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://llmounir:'+ encodeURIComponent('PN3mAEYzLdyVaee') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
